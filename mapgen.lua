214a4d5236a02a888e449a73bdb24b6dfe99a644
SmallJoker <st.rentsch@hotmail.com>
2014-01-16 18:56:10 +0100

    Alternative fast-generating
    A code clean-up

--- mapgen.lua
+++ mapgen.lua
@@ -21,18 +21,17 @@ minetest.register_on_generated(function(minp, maxp, seed)
 		return
 	end
 	local stop = false
-	for px=2,maxp.x-minp.x-2 do
-	for pz=2,maxp.z-minp.z-2 do
-		local cpos = {x=minp.x+px,y=1,z=minp.z+pz}
-		local cname = minetest.get_node(cpos).name
+	for px=minp.x+2,maxp.x-2 do
+	for pz=minp.z+2,maxp.z-2 do
+		local cname = minetest.get_node({x=px,y=1,z=pz}).name
 		if(cname == "default:desert_sand") then
 			-- AAH! Too hot!
 			stop = true
 			break
 		end
 		if(cname == "default:dirt_with_grass" and math.random(1,15) == 2) then
-			if minetest.find_node_near(cpos, 2, "default:water_source") then
-				make_bamboo({x=minp.x+px,y=2,z=minp.z+pz}, math.random(3, 6))
+			if minetest.find_node_near({x=px,y=1,z=pz}, 2, "default:water_source") then
+				make_bamboo({x=px,y=2,z=pz}, math.random(3, 6))
 			end
 		end
 	end
