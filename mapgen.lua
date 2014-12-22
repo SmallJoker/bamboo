1d7920622e969cb7cbedf8df8c7df1161fea9ab7
SmallJoker <mk939@ymail.com>
2014-12-22 16:28:23 +0100

    The same with mapgen.lua

--- mapgen.lua
+++ mapgen.lua
@@ -1,10 +1,10 @@
 function make_bamboo(pos, size)
-	for y=0,size-1 do
+	for y=0, size-1 do
 		local p = {x=pos.x, y=pos.y+y, z=pos.z}
-		local nn = minetest.get_node(p).name
-		if nn == "air" then
+		local node = minetest.get_node(p).name
+		if node == "air" then
 			minetest.set_node(p, {name="bamboo:bamboo"})
-		elseif (nn == "default:dirt" or nn == "default:dirt_with_grass") then
+		elseif node == "default:dirt" or name == "default:dirt_with_grass" then
 			return
 		else
 			break
@@ -13,30 +13,28 @@ function make_bamboo(pos, size)
 end
 
 minetest.register_on_generated(function(minp, maxp, seed)
-	if(minp.y < -35 or maxp.y > 50) then
+	if minp.y < -35 or maxp.y > 50 then
 		return
 	end
-	if(math.random(1,5) ~= 2) then
+	if math.random(1, 4) ~= 2 then
 		-- Making rare...
 		return
 	end
-	local stop = false
-	for px=minp.x+2,maxp.x-2 do
-	for pz=minp.z+2,maxp.z-2 do
-		local cname = minetest.get_node({x=px,y=1,z=pz}).name
-		if(cname == "default:desert_sand") then
+	
+	for px = minp.x+2, maxp.x-2 do
+	for pz = minp.z+2, maxp.z-2 do
+		local pos = {x=px, y=1, z=pz}
+		local node = minetest.get_node(pos).name
+		if node == "default:desert_sand" then
 			-- AAH! Too hot!
-			stop = true
-			break
+			return
 		end
-		if(cname == "default:dirt_with_grass" and math.random(1,12) == 2) then
-			if minetest.find_node_near({x=px,y=1,z=pz}, 2, "default:water_source") then
-				make_bamboo({x=px,y=2,z=pz}, math.random(3, 6))
+		if node == "default:dirt_with_grass" and math.random(1, 12) == 2 then
+			if minetest.find_node_near(pos, 3, "default:water_source") then
+				pos.y = pos.y + 1
+				make_bamboo(pos, math.random(3, 6))
 			end
 		end
 	end
-		if(stop) then
-			break
-		end
 	end
 end)
